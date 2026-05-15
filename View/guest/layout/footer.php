9b1610e56d3fcaca48b4c9d4b84c1288a07c2c86
tamjidniloy <tamjidniloy22@gmail.com>
2026-05-15 16:14:49 +0600

    Guest front end is updated

---
+++ View/guest/layout/footer.php
@@ -0,0 +1,13 @@
+</div> </div> <div class="toast-container position-fixed bottom-0 end-0 p-3">
+    <div id="appToast" class="toast align-items-center text-white bg-dark border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+            <div class="toast-body" id="toastMessage"></div>
+            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/text/js/bootstrap.bundle.min.js"></script>
+<script src="../../public/assets/js/guest.js"></script>
+</body>
+</html>
