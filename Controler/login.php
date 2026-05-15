b0c221fdbebcecb466a6eda6a8689444a6ed57f6
tamjidniloy <tamjidniloy22@gmail.com>
2026-05-15 15:53:10 +0600

    Changed in login functionality

--- Controler/login.php
+++ Controler/login.php
@@ -2,20 +2,43 @@
 include("../Model/db.php");
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
     $username = $_POST["username"] ?? "";
     $password = $_POST["password"] ?? "";
 
-    $data = db::Fetch("select id, password_hash, role from users where email=? or phone=? or username=?", $username, $username, $username);
-    if($data != Null)
-    {
+    $data = db::Fetch("SELECT id, password_hash, role FROM users WHERE email=? OR phone=? OR username=?",$username, $username, $username);
+
+    if($data != null){
+
         if($password == $data["password_hash"])
         {
+            $role = $data["role"]; // store role in variable
+            $id = $data["id"];
+
+            // common cookie (optional)
             if(isset($_POST["rememberMe"]))
             {
-                setcookie("id", $data["id"], time() + (86400 * 30), "/");
-                setcookie("nextPage", "../View/" . $data["role"] . ".html", time() + (86400 * 30), "/");
+                setcookie("user_id", $id, time() + (86400 * 30), "/");
+                setcookie("role", $role, time() + (86400 * 30), "/");
             }
-            header("Location: ../View/" . $data["role"] . ".html");
+
+            // role-based routing
+            if($role == "admin"){
+                header("Location: ../View/admin.html");
+            }
+            else if($role == "guest"){
+                header("Location: ../View/guest/guest_dashboard.php");
+            }
+            else if($role == "housekeeping"){
+                header("Location: ../View/housekeeping.html");
+            }
+            else if($role == "receptionist"){
+                header("Location: ../View/receptionist.html");
+            }
+            else{
+                header("Location: ../View/login.html?error=invalid_role");
+            }
+
             exit();
         }
         else
